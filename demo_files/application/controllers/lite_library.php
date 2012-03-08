36ba6e56a266f5e422dd8e1527b58dd3e3e53d47
Rob Hussey <rob@haseydesign.com>
2012-03-08 22:55:01 +1100

    Updated "base_url" and "includes_dir" of demo controllers to generic directory names

--- demo_files/application/controllers/lite_library.php
+++ demo_files/application/controllers/lite_library.php
@@ -31,8 +31,8 @@ class Lite_library extends CI_Controller {
 		$this->load->library('flexi_cart_lite', FALSE, 'flexi_cart');	
 		
 		// Note: This is only included to create base urls for purposes of this demo only and are not necessarily considered as 'Best practice'.
-		$this->load->vars('base_url', 'http://localhost/Flexi/flexi_cart/');
-		$this->load->vars('includes_dir', 'http://localhost/Flexi/flexi_cart/includes/');
+		$this->load->vars('base_url', 'http://localhost/your_codeigniter_directory/');
+		$this->load->vars('includes_dir', 'http://localhost/your_codeigniter_directory/includes/');
 		$this->load->vars('current_url', $this->uri->uri_to_assoc(1));
 		
 		// Load cart data to be displayed via 'Mini Cart' menu.
