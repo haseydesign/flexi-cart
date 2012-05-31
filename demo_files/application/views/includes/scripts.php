258d996ae09973370be42ded5c6ff0fdb40d18eb
Rob Hussey <rob@haseydesign.com>
2012-05-31 23:05:52 +1000

    Updated jQuery script to 1.7.2

--- demo_files/application/views/includes/scripts.php
+++ demo_files/application/views/includes/scripts.php
@@ -1,5 +1,5 @@
 <!-- JS Includes -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="<?php echo $includes_dir;?>js/jquery.tools.tooltips.min.js?v=1.0"></script>
 
 <script src="<?php echo $includes_dir;?>js/global.js?v=1.0"></script>
