446ce13d627b23565ce81ba4b369cd99c3544a46
Rob Hussey <rob@haseydesign.com>
2012-08-16 22:02:44 +1000

    Added new page to user guide detailing admin cart functions.

--- demo_files/application/views/includes/head.php
+++
@@ -1,7 +0,0 @@
-	<meta name="robots" content="index, follow"/>
-	<meta name="designer" content="haseydesign - Rob Hussey : rob @ haseydesign .com"/> 
-	<meta name="copyright" content="Copyright &copy; <?php echo date('Y');?>, Rob Hussey, All Rights Reserved"/>
-	<meta http-equiv="imagetoolbar" content="no"/>	
-	
-	<link rel="stylesheet" href="<?php echo $includes_dir;?>css/global.css?v=0.1">
-	<link rel="stylesheet" href="<?php echo $includes_dir;?>css/structure.css?v=0.1">
--- demo_files/application/views/includes/scripts.php
+++
@@ -1,8 +0,0 @@
-<!-- JS Includes -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script src="<?php echo $includes_dir;?>js/jquery.tools.tooltips.min.js?v=1.0"></script>
-
-<script src="<?php echo $includes_dir;?>js/global.js?v=1.0"></script>
-<?php if (isset($current_url['admin_library']) || isset($current_url['user_guide'])) { ?>
-<script src="<?php echo $includes_dir;?>js/admin_global.js?v=1.0"></script>
-<?php } ?>
